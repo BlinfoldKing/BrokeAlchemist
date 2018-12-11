6f2cb0ddce3028422e3fb66790414bd1df7045cb
Ganesha Danu <ganeshadanu1@gmail.com>
2018-12-12 02:21:37 +0700

    add potion 1

---
+++ Potion_1_FizzBuzz/FizzBuzz.ex
@@ -0,0 +1,16 @@
+defmodule FizzBuzz do
+  def evaluate x do
+    getFizz(x, [ rem(x, 3), rem(x, 5), rem(x, 15) ])
+  end
+
+  defp getFizz(x, lst) do
+    case lst do
+      [ 0, 0, 0 ] -> "FizzBuzz"
+      [ 0, _, _ ] -> "Fizz"
+      [ _, 0, _ ] -> "Buzz"
+      [ _, _, _ ] -> x
+    end
+  end
+end
+
+Enum.map(1..100, fn x -> IO.puts FizzBuzz.evaluate x end)
